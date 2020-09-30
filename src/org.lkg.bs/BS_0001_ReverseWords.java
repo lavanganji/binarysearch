45272e70e1dea81f28f04b7186e2bcbda1aabc78
Lavan Kumar <lavanganji@gmail.com>
2020-09-29 20:16:08 -0500

    Creation java Prj

---
+++ src/org.lkg.bs/BS_0001_ReverseWords.java
@@ -0,0 +1,14 @@
+package org.lkg.bs;
+
+import java.util.*;
+
+public class BS_0001_ReverseWords {
+
+
+	public static void main(String[] args) {
+		String result ="Hi";
+    	//String result = solve("hello there my friend");
+		System.out.println("Reverse Words :"+result);    
+    }
+}
+
